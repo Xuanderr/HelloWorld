a9fd5fe9c0152a132dccbcbdf91e6a05bdf7438f
l00lirik <95029389+l00lirik@users.noreply.github.com>
2021-12-18 01:15:08 +0400

    Delete src/main/resources/database directory
    Initial commit

--- src/main/resources/database/initDB.sql
+++
@@ -1,7 +0,0 @@
---CREATE TABLE IF NOT EXISTS clients
---(
---    id  BIGSERIAL PRIMARY KEY ,
---    user_name  text NOT NULL ,
---    email text NOT NULL ,
---    phone text  NOT NULL
---);
--- src/main/resources/database/populateDB.sql
+++
@@ -1,3 +0,0 @@
---INSERT INTO clients VALUES
---('Vassily Petrov', 'vpetrov@jr.com', '+7 (191) 322-22-33)'),
---('Pjotr Vasechkin', 'pvasechkin@jr.com', '+7 (191) 223-33-22)');
