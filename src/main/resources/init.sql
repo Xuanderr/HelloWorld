258f09e127b1b14d40cae32cb43ac16d951ec0ae
l00lirik <pozdeevk1999@gmail.com>
2022-02-18 23:20:22 +0400

    Initial commit

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -21,8 +21,6 @@
 --    full_text text NOT NULL,
 --    user_id bigint NOT NULL,
 --    created_date_time timestamp without time zone,
---    likes integer,
---    reposts integer,
 --    views integer,
 --    CONSTRAINT article_pkey PRIMARY KEY (article_id),
 --    CONSTRAINT users_fkey FOREIGN KEY (user_id) REFERENCES users (user_id)
@@ -46,7 +44,7 @@
 --    CONSTRAINT tags_pkey PRIMARY KEY (id),
 --    CONSTRAINT tag_name UNIQUE (name)
 --);
---CREATE TABLE IF NOT EXISTS article_tags
+--CREATE TABLE IF NOT EXISTS article_tag
 --(
 --    article_id bigint NOT NULL,
 --    tag_id bigint NOT NULL,
@@ -54,8 +52,19 @@
 --    CONSTRAINT articles_fkey FOREIGN KEY (article_id) REFERENCES articles (article_id),
 --    CONSTRAINT tags_fkey FOREIGN KEY (tag_id) REFERENCES tags (id)
 --);
+--CREATE TABLE IF NOT EXISTS likes
+--(
+--    like_id bigserial NOT NULL,
+--    article_id bigint NOT NULL,
+--    user_id bigint NOT NULL,
+--    CONSTRAINT likes_pkey PRIMARY KEY (like_id),
+--    CONSTRAINT "uniqueUserAndArticle" UNIQUE (user_id, article_id),
+--    CONSTRAINT articles_fkey FOREIGN KEY (article_id) REFERENCES articles (article_id),
+--    CONSTRAINT users_fkey FOREIGN KEY (user_id) REFERENCES users (user_id)
+--);
 
 --Part 2: For clean tables
+--delete from likes;
 --delete from article_tags;
 --delete from tags;
 --delete from comments;
@@ -69,10 +78,10 @@ INSERT INTO users(name, email, password, role, status, created_date_time) VALUES
 INSERT INTO users(name, email, password, role, status, created_date_time) VALUES('admin', 'admin@mail.ru', '$2a$12$ChfGOXVH0SoUcMrBRYRcmO39PPjYmouU4aLZiyzsy4dwNCYn7Ucwm', 'ADMIN', 'ACTIVE', '2015-12-01 12:05:00');
 INSERT INTO users(name, email, password, role, status, created_date_time) VALUES('eva', 'eva@mail.ru', '$2a$12$BEfybZRlCsX43bDSK4BPle/k6d7ai4n.QoyKRCoJ8uOdfAtpWekI2', 'USER', 'ACTIVE', '2017-06-15 12:05:00');
 
-INSERT INTO articles(title, anons, full_text, user_id, created_date_time, likes, reposts, views) VALUES('first', 'about_first', 'text_for_first', 1, '2016-08-10 12:05:00', 2, 1, 10);
-INSERT INTO articles(title, anons, full_text, user_id, created_date_time, likes, reposts, views) VALUES('second', 'about_second', 'text_for_second', 1, '2016-12-25 12:05:00', 1, 0, 4);
-INSERT INTO articles(title, anons, full_text, user_id, created_date_time, likes, reposts, views) VALUES('third', 'about_third', 'text_for_third', 2, '2018-03-17 12:05:00', 5, 0, 20);
-INSERT INTO articles(title, anons, full_text, user_id, created_date_time, likes, reposts, views) VALUES('fourth', 'about_fourth', 'text_for_fourth', 4, '2019-01-01 12:05:00', 3, 7, 32);
+INSERT INTO articles(title, anons, full_text, user_id, created_date_time, views) VALUES('first', 'about_first', 'text_for_first', 1, '2016-08-10 12:05:00', 10);
+INSERT INTO articles(title, anons, full_text, user_id, created_date_time, views) VALUES('second', 'about_second', 'text_for_second', 1, '2016-12-25 12:05:00', 4);
+INSERT INTO articles(title, anons, full_text, user_id, created_date_time, views) VALUES('third', 'about_third', 'text_for_third', 2, '2018-03-17 12:05:00', 20);
+INSERT INTO articles(title, anons, full_text, user_id, created_date_time, views) VALUES('fourth', 'about_fourth', 'text_for_fourth', 4, '2019-01-01 12:05:00', 32);
 
 INSERT INTO comments(article_id, user_id, full_text, created_date_time) VALUES(1, 1, 'comment1', '2016-08-15 12:05:00');
 INSERT INTO comments(article_id, user_id, full_text, created_date_time) VALUES(1, 2, 'comment2', '2016-09-20 12:05:00');
@@ -89,13 +98,25 @@ INSERT INTO tags(name) VALUES('tag3');
 INSERT INTO tags(name) VALUES('tag4');
 INSERT INTO tags(name) VALUES('tag5');
 
-INSERT INTO article_tags(article_id, tag_id) VALUES(1, 1);
-INSERT INTO article_tags(article_id, tag_id) VALUES(1, 2);
-INSERT INTO article_tags(article_id, tag_id) VALUES(1, 3);
-INSERT INTO article_tags(article_id, tag_id) VALUES(2, 1);
-INSERT INTO article_tags(article_id, tag_id) VALUES(3, 1);
-INSERT INTO article_tags(article_id, tag_id) VALUES(4, 4);
-INSERT INTO article_tags(article_id, tag_id) VALUES(4, 5);
+INSERT INTO article_tag(article_id, tag_id) VALUES(1, 1);
+INSERT INTO article_tag(article_id, tag_id) VALUES(1, 2);
+INSERT INTO article_tag(article_id, tag_id) VALUES(1, 3);
+INSERT INTO article_tag(article_id, tag_id) VALUES(2, 1);
+INSERT INTO article_tag(article_id, tag_id) VALUES(3, 1);
+INSERT INTO article_tag(article_id, tag_id) VALUES(4, 4);
+INSERT INTO article_tag(article_id, tag_id) VALUES(4, 5);
+
+INSERT INTO likes(like_id, user_id, article_id) VALUES(1, 1, 1);
+INSERT INTO likes(like_id, user_id, article_id) VALUES(2, 1, 2);
+INSERT INTO likes(like_id, user_id, article_id) VALUES(3, 1, 3);
+INSERT INTO likes(like_id, user_id, article_id) VALUES(4, 1, 4);
+INSERT INTO likes(like_id, user_id, article_id) VALUES(5, 2, 1);
+INSERT INTO likes(like_id, user_id, article_id) VALUES(6, 2, 4);
+INSERT INTO likes(like_id, user_id, article_id) VALUES(7, 3, 1);
+INSERT INTO likes(like_id, user_id, article_id) VALUES(8, 3, 2);
+INSERT INTO likes(like_id, user_id, article_id) VALUES(9, 3, 4);
+INSERT INTO likes(like_id, user_id, article_id) VALUES(10, 4, 3);
+INSERT INTO likes(like_id, user_id, article_id) VALUES(11, 4, 4);
 
 
 
